0c80bedffad11c3fbff639de16363da5d10fca82
errnull <errnull@libsvg.com>
2023-04-03 16:26:16 +0800

    feat: (2-1) imp selection sort int.

---
+++ SelectionSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void swapInt(int &a, int &b)
+{
+    if (&a != &b)
+    {
+        a ^= b;
+        b ^= a;
+        a ^= b;
+    }
+}
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // 寻找[i， n]区间里的最小值
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        swapInt(arr[i], arr[minIndex]);
+    }
+}
+
+int main()
+{
+    int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    selectionSort(a, 10);
+    for(int i = 0; i < 10; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
