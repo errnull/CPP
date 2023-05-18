755ca6601e8106c7dd78feeb55653afc55804acf
errnull <errnull@libsvg.com>
2023-05-18 23:52:47 +0800

    feat: upfate imp merge two lists.

---
+++ list_node/MergeTwoLists.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // 虚拟头节点
+        ListNode *dummy = new ListNode(-1), *p = dummy;
+        ListNode *p1 = l1, *p2 = l2;
+
+        while (p1 != NULL && p2 != NULL) {
+            // 比较 p1 和 p2 两个指针
+            // 将值较小的节点连接到 p 指针
+            if (p1->val > p2->val) {
+                p->next = p2;
+            } else {
+                p->next = p1;
+                p1 = p1->next;
+            }
+            // p 指针不断前进
+            p = p->next;
+        }
+
+        if (p1 != NULL) {
+            p->next = p1;
+        }
+        if (p2 != NULL) {
+            p->next = p2;
+        }
+
+        return dummy->next;
+    }
+};
+
+// 测试代码
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Solution solution;
+
+    ListNode* l1 = new ListNode(1);
+    l1->next = new ListNode(2);
+    l1->next->next = new ListNode(4);
+
+    ListNode* l2 = new ListNode(1);
+    l2->next = new ListNode(3);
+    l2->next->next = new ListNode(4);
+
+    ListNode* mergedList = solution.mergeTwoLists(l1, l2);
+
+    printList(mergedList);
+
+    return 0;
+}
